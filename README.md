101e7404ce73d3137c8cec025f717aa5748d216b
Artem Aleksandruk <artem.alek@icloud.com>
2017-02-12 17:27:36 -0500

    working on more css and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#Making an accordion using jquery
+#Dynamically creating product boxes and using Esty API data to sort.
 
-Link to project https://artem-alek.github.io/jquery-accordion/
+Link to project https://artem-alek.github.io/weekend-3-assignment/
 
-###Used jQuery methods .addClass() & .removeClass()
-###learned to use transition property in css.
+###Learned to use complex Objects and use Jquery to pull data.
+###Used functions and JQuery and template ileterate to make templates for the data
--- jscript/main.js
+++ jscript/main.js
@@ -1,9 +1,32 @@
-function productGenerate () {
-  for (i=0; i < etsyData.results.length; i++) {
-    $('.product').eq(i).html(`<div class='pImage '> <img src='${etsyData.results[i].Images[0].url_170x135}'/></div>
-    <p class='title'>${etsyData.results[i].title}</p><p class='shop left'>${etsyData.results[i].Shop.login_name}
-    </p><p class='price right'>${etsyData.results[i].price}</p>`)
+// function productGenerate () {
+//   for (i=0; i < etsyData.results.length; i++) {
+//     $('.product').eq(i).html(`<div class='pImage '> <img src='${etsyData.results[i].Images[0].url_170x135}'/></div>
+//     <p class='title'>${etsyData.results[i].title}</p><p class='shop left'>${etsyData.results[i].Shop.login_name}
+//     </p><p class='price right'>${etsyData.results[i].price}</p>`)
+//   }
+// }
+//
+// productGenerate()
+
+function createBox (x) {
+  return `
+          <div class='product'>
+            <div class="pImage">
+              <img src='${x.Images[0].url_170x135}'/>
+            </div>
+            <p class='title'>${x.title}</p>
+            <p class='shop left'>${x.Shop.login_name}</p>
+            <p class='price right'>${x.price}</p>
+          </div>
+        `
+}
+
+function makeTemplate () {
+  for (var i = 0; i < 16; i++) {
+    var result = etsyData.results[i];
+    var makeHtml = createBox(result);
+    $('.productsCont').append(makeHtml);
   }
 }
 
-productGenerate()
+makeTemplate();
